e14c30a0bc58ccae7b19ce0f97d3f61ea434af71
Carlos Adrian Guerra Vazquez <cguerravazqu@microsoft.com>
2022-12-02 10:00:21 -0800

    added day one and day two code for 2022

---
+++ AdventOfCode2022/Day1.1-Calories_Counting/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Day1_Calories_Counting
+{
+    internal class Program
+    {
+        private const string filePath = "input.txt";
+
+        void FileReader(string filePath, Action<String> loopHandler)
+        {
+            try
+            {
+                using StreamReader reader = new(filePath);
+                String? line;
+                while ((line = reader?.ReadLine()) != null)
+                {
+                    loopHandler(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        void PartOne()
+        {
+            int currentCalories = 0;
+            int maxCalories = 0;
+
+            FileReader(filePath, (string line) =>
+            {
+                if (String.IsNullOrWhiteSpace(line))
+                {
+                    maxCalories = currentCalories > maxCalories ? currentCalories : maxCalories;
+                    currentCalories = 0;
+                }
+                else
+                {
+                    currentCalories += int.Parse(line);
+                }
+            });
+            maxCalories = currentCalories > maxCalories ? currentCalories : maxCalories;
+            Console.WriteLine(maxCalories);
+        }
+
+        void PartTwo()
+        {
+            int[] top3MaxCalories = { 0, 0, 0 };
+            int currentCalories = 0;
+
+            void CompareAndUpdateMax()
+            {
+                int biggestDiference = 0;
+                int biggestDiferenceIndex = -1;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    int difference = currentCalories - top3MaxCalories[i];
+                    if (difference > 0 && difference > biggestDiference)
+                    {
+                        biggestDiference = difference;
+                        biggestDiferenceIndex = i;
+                    }
+                }
+                if (biggestDiferenceIndex >= 0)
+                {
+                    top3MaxCalories[biggestDiferenceIndex] = currentCalories;
+                }
+                currentCalories = 0;
+            }
+
+            FileReader(filePath, (string line) =>
+            {
+                if (String.IsNullOrWhiteSpace(line))
+                {
+                    CompareAndUpdateMax();
+                }
+                else
+                {
+                    currentCalories += int.Parse(line);
+                }
+            });
+            CompareAndUpdateMax();
+            Console.WriteLine(top3MaxCalories.Sum());
+        }
+
+        static void Main(string[] args)
+        {
+            new Program().PartTwo();
+        }
+    }
+}
---
+++ AdventOfCode2022/Day2_RockPaperScissors/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Day2_RockPaperScissors
+{
+    internal class Program
+    {
+        private const string filePath = "input.txt";
+
+        private static int[,] matchTable = new int[,]{
+               //X,   Y,   Z
+          /*A*/ {1+3, 2+6, 3+0},
+          /*B*/ {1+0, 2+3, 3+6},
+          /*C*/ {1+6, 2+0, 3+3},
+            };
+
+        private static int[,] matchTable2 = new int[,]{
+               //X,   Y,   Z
+          /*A*/ {3+0, 1+3, 2+6},
+          /*B*/ {1+0, 2+3, 3+6},
+          /*C*/ {2+0, 3+3, 1+6},
+            };
+
+        static void FileReader(string filePath, Action<String> loopHandler)
+        {
+            try
+            {
+                using StreamReader reader = new(filePath);
+                String? line;
+                while ((line = reader?.ReadLine()) != null)
+                {
+                    loopHandler(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static int StrToInt(string str)
+        {
+            char c = (char)str[0];
+            if (c > 'C')
+            {
+                return c - 'X';
+            }
+            else
+            {
+                return c - 'A';
+            }
+        }
+
+        void Run(int[,] matchTable)
+        {
+            int sum = 0;
+            FileReader(filePath, (string line) =>
+            { 
+                string[] inputs = line.Split(' ');
+                int oponent = StrToInt(inputs[0]);
+                int player = StrToInt(inputs[1]);
+
+                if(oponent > 2 || oponent < 0 || player > 2 || player < 0)
+                {
+                    throw new Exception("##Invalid play index");
+                }
+
+                sum += matchTable[oponent,player];
+            });
+            Console.WriteLine(sum);
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new();
+            p.Run(matchTable);//part1
+            p.Run(matchTable2);//part2
+        }
+    }
+}
