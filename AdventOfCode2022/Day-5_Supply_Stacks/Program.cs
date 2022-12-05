a9a43c8c66aa8473128379be18731fc081b8e356
Carlos Adrian Guerra Vazquez <cguerravazqu@microsoft.com>
2022-12-05 14:51:53 -0800

    added day5 code

--- AdventOfCode2022/Day-5_Supply_Stacks/Program.cs
+++ AdventOfCode2022/Day-5_Supply_Stacks/Program.cs
@@ -1,8 +1,10 @@
-﻿namespace Day_5_Supply_Stacks
+﻿using System.Text.RegularExpressions;
+
+namespace Day_5_Supply_Stacks
 {
     internal class Program
     {
-        private const string filePath = "input2.txt";
+        private const string filePath = "input.txt";
 
         static void FileReader(string filePath, Action<String, int> loopHandler)
         {
@@ -23,14 +25,111 @@
             }
         }
 
+        void ReadStackLine(Stack<char>[] stacks, string line, int stacksNum)
+        {
+            for(int i =0; i < stacksNum; i++) 
+            {
+                char c = line[(i * 4) + 1];
+                if (c == '1') return;
+                if (c != 32) stacks[i].Push(c);
+            }
+        }
+
+        Stack<char> ReverseStack(Stack<char> stack) 
+        {
+            Stack<char> temp = new();
+            while (stack.Count != 0)
+            {
+                temp.Push(stack.Pop());
+            }
+            return temp;
+        }
+
+        int[] ParseRule(String line)
+        {
+            string[] parsed = Regex.Split(line, @"\D+");
+            parsed = parsed.Skip(1).ToArray();
+            int[] values = Array.ConvertAll(parsed, (s) => int.Parse(s));
+
+            return values;
+        }
+
+        void RunRulePart1(int[] rule, Stack<char>[] stacks)
+        {
+            for (int i = 0; i < rule[0]; i++)
+            {
+                stacks[rule[2] - 1].Push(stacks[rule[1] - 1].Pop());
+            }
+        }
+
+        void RunRulePart2(int[] rule, Stack<char>[] stacks)
+        {
+            Stack<char> temp = new();
+
+            for (int i = 0; i < rule[0]; i++)
+            {
+                temp.Push(stacks[rule[1] - 1].Pop());
+            }
+            for (int i = 0; i < rule[0]; i++)
+            {
+                stacks[rule[2] - 1].Push(temp.Pop());
+            }
+        }
+
         void Part1()
         {
-            int sum = 0;
+            string result = "";
+            int stacksNum = 0;
+            bool readingRules = false;
+            Stack<char>[] stacks = null;
+
             FileReader(filePath, (line, index) =>
             {
-                
+                if (!String.IsNullOrWhiteSpace(line))
+                {
+                    if (!readingRules)
+                    {
+                        if (index == 0)
+                        {
+                            stacksNum = (line.Length + 1) / 4;
+
+                            //initialize stacks
+                            stacks = new Stack<char>[stacksNum];
+                            for (int i = 0; i < stacksNum; i++)
+                            {
+                                stacks[i] = new();
+                            }
+                        }
+
+                        ReadStackLine(stacks, line, stacksNum);
+                    }
+                    else
+                    {
+                        int[] rule = ParseRule(line);
+
+                        //RunRulePart1(rule, stacks);
+                        RunRulePart2(rule, stacks);
+                    }
+                }
+                else
+                {
+                    if (!readingRules)
+                    {
+                        readingRules = true;
+                        for (int i = 0; i < stacksNum; i++)
+                        {
+                            stacks[i] = ReverseStack(stacks[i]);
+                        }
+                    }
+                }
             });
-            Console.WriteLine(sum);
+
+            for (int i = 0; i < stacksNum; i++)
+            {
+                result += stacks[i].Peek();
+            }
+
+            Console.WriteLine(result);
         }
 
         static void Main(string[] args)
